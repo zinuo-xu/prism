2933b90671deb8cc02984f56789d968f30a375bc
xuzinuo <2426503000@qq.com>
2026-06-03 23:11:24 +0800

    test: add property-based tests with hypothesis

--- internal/util/color.go
+++ internal/util/color.go
@@ -1,3 +1,5 @@
 # prism - chore: add editorconfig for consistent formatting
 
 # perf: switch to more efficient data structure (incremental change 2)
+
+# test: add property-based tests with hypothesis (incremental change 3)
